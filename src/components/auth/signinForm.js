ab3ead5dac1f8230377ee1874fc72712ab971b41
Spencer Suckow <ssuckow05@gmail.com>
2022-10-11 10:35:04 -0600

    styled form fields

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
- 
+
 import { FormTitle } from '../formTitle';
 import { FormInput } from '../formFields';
 
@@ -8,8 +8,10 @@ class SigninForm extends Component {
     render() {
         return (
             <form className='sign-in-form'>
-                <FormTitle className='sign-in__title' text='Login'/>
-                <Field className='sign-in-form__email' component={FormInput} name='email' type='email'/>
+                <FormTitle className='sign-in-form__title' text='Login' />
+                <Field className='sign-in-form__email' placeholder='Enter Email' component={FormInput} name='email' type='email' title='Email'/>
+                <Field className='sign-in-form__password' placeholder='Enter Password' component={FormInput} name='password' type='password' title='Password'/>
+
             </form>
         );
     }
