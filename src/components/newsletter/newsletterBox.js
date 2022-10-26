de0749e48cc97e7d4db464767904750b8699edd2
Spencer Suckow <ssuckow05@gmail.com>
2022-10-26 10:52:12 -0600

    styled newsletterbox componenet

--- src/components/newsletter/newsletterBox.js
+++ src/components/newsletter/newsletterBox.js
@@ -5,8 +5,8 @@ const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov
 export default function NewsletterBox({date}) {
         return (
             <div className='newsletter-box'>
-                <div className='newsletter-box__day'>{props.date.getDate()}</div>
-                <div className='newsletter-box__month-year'>{months[props.date.getMonth()]} {props.date.getFullYear()}</div>
+                <div className='newsletter-box__day'>{date.getDate()}</div>
+                <div className='newsletter-box__month-year'>{months[date.getMonth()]} {date.getFullYear()}</div>
 
             </div>
         );
