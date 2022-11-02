5a20904ecfe55cd4f59202afd3e1e05f19f39535
Spencer Suckow <ssuckow05@gmail.com>
2022-11-02 11:11:27 -0600

    newsletter-new-form and placement on grid

--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -11,15 +11,9 @@ class NewNewsletterForm extends Component {
         const { handleSubmit } = this.props;
 
         return (
-            <form onSubmit={handleSubmit} className='sign-in-form'>
-                <FormTitle className='sign-in-form__title' text='Login' />
-                <Field className='sign-in-form__email' placeholder='Enter Email' component={FormInput} name='email' type='email' title='Email' />
-                <Field className='sign-in-form__password' placeholder='Enter Password' component={FormInput} name='password' type='password' title='Password' />
-                <Field className='sign-in-form__login' component={FormButton} name='login' type='submit' title='Login' />
-                <div className='sign-in-form__text-links'>
-                    <TextLink to='/forgot' text='Forgot Password' />
-                    <TextLink to='/signup' text='Not a Member? Register here' />
-                </div>
+            <form onSubmit={handleSubmit} className='new-newsletter-form'>
+                <FormTitle className='new-newsletter-form__title' text='Login' />
+                <Field className='new-newsletter-form__email' placeholder='Enter Email' component={FormInput} name='email' type='email' title='Email' />
             </form>
         );
     }
