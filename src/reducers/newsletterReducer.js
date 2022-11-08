dfe7635957fc0edd2f69819a3e00bcfd258a7d21
Spencer Suckow <ssuckow05@gmail.com>
2022-11-08 10:27:13 -0700

    built fetchnewsletterwithid action and reducer

--- src/reducers/newsletterReducer.js
+++ src/reducers/newsletterReducer.js
@@ -1,10 +1,12 @@
 import {
+    FETCH_NEWSLETTER_ID,
     SET_NEWSLETTERS
 } from '../actions/types';
 
 
 const INITIAL_STATE = {
-    newsletters: []
+    newsletters: [],
+    newsletterToEdit: {}
 }
 
 export default function(state = INITIAL_STATE, action) {
@@ -15,7 +17,18 @@ export default function(state = INITIAL_STATE, action) {
                 ...state,
                 newsletters
             }
-    
+        case FETCH_NEWSLETTER_ID:
+            const newsletterID = action.payload;
+            var newsletterToEdit = {};
+            state.newsletters.map(newsletter => {
+                if(newsletter._id == newsletterID) {
+                    newsletterToEdit = newsletter;
+                }
+            })
+            return {
+                ...state,
+                newsletterToEdit
+            }
         default: return state;
         
     }
