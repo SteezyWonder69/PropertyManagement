044a8d2ac89677473c879f612088142c2a1c2747
Spencer Suckow <ssuckow05@gmail.com>
2022-11-15 10:26:33 -0700

    laid out jsx on requests item grid

--- src/components/requests/requestsGrid.js
+++ src/components/requests/requestsGrid.js
@@ -14,7 +14,7 @@ export default class RequestsGrid extends Component {
     render() {
         return (
             <div className='requests-grid'>
-                <Button className='requests-grid__button' callback={() => this.handleAddRequest()} icon="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
+                <Button className='requests-grid__button' callback={() => this.handleAddRequest()} icon="fas fa-plus"/>
                 <RequestsBoxes/>
                 <Requests/>
             </div>
