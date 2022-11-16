74a88b13bcb7962ccaae891bf3f2022f8f6f9fcb
Spencer Suckow <ssuckow05@gmail.com>
2022-11-16 10:20:49 -0700

    installed react-animate-height and modified icon component to take in a custom callback for added functionality

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,31 +1,59 @@
 import React, { Component } from 'react';
 
+import AnimateHeight from 'react-animate-height';
+
 import Icon from '../icon';
 import Button from '../button';
 
 export default class RequestsItem extends Component {
+
+    constructor() {
+        super() 
+
+        this.state = {
+            height: 0
+        }
+    }
+
+    toggleDropdown = () => {
+        if(this.state.height == 0) {
+            this.setState({height: 'auto'})
+        } else {
+            this.setState({height: 0})
+        }
+    }
+
     render() {
         return (
             <div className='requests-item'>
                 <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle' />
                 <div className='requests-item__title'>
                     <div className='requests-item__title__text'>Sick title bro</div>
-                    <Icon className='requests-item__title__arrow' icon='fas fa-sort-down' />
+                    <Icon callback={() => this.toggleDropdown()} className='requests-item__title__arrow' icon='fas fa-sort-down' />
                 </div>
                 <div className='requests-item__tenant-unit'>
                     Spencer - Unit 318
                 </div>
-                
+
                 <div className='requests-item__date'>23/5/1005</div>
                 <Button className='requests-item__move' icon='fas fa-wrench' callback={() => conosole.log('move buddy')} />
+
                 <div className='requests-item__description'>
-                    <img
-                        src='http://via.placeholder.com/160x94'
-                        className='requests-item__description-img' />
-                    <p className='requests-item__description-text'>
-                        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum 
-                    </p>
+                    <AnimateHeight
+                        duration={300}
+                        height={this.state.height}
+                    >
+                        <div className='requests-item__description'>
+                            <img
+                                src='http://via.placeholder.com/160x94'
+                                className='requests-item__description-img' />
+                            <p className='requests-item__description-text'>
+                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                            </p>
+                        </div>
+                    </AnimateHeight>
                 </div>
+
             </div>
         );
     }
